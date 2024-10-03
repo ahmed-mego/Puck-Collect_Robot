bd8284702f874d2008785410b2f571d68526236f
Ahmed Magdy <124188693+ahmego@users.noreply.github.com>
2024-10-04 01:43:19 +0300

    Delete raspberryPi_Code directory
    Signed-off-by: Ahmed Magdy <124188693+ahmego@users.noreply.github.com>

--- raspberryPi_Code/ros_ws/build/ctrl_interface/CMakeFiles/ctrl_interface__rosidl_generator_type_description.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ctrl_interface__rosidl_generator_type_description"
-  "rosidl_generator_type_description/ctrl_interface/msg/CtrlInterface.json"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ctrl_interface__rosidl_generator_type_description.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- raspberryPi_Code/ros_ws/install/ctrl_interface/lib/python3.12/site-packages/ctrl_interface/msg/__init__.py
+++
@@ -1,1 +0,0 @@
-from ctrl_interface.msg._ctrl_interface import CtrlInterface  # noqa: F401
